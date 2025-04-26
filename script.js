3841936c64ee37de799c6ddb50bc4f17651b85cb
--replace-all <mrakkus15@gmail.com>
2025-04-26 22:23:31 +0300

    feat: animation for slider done

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let rightArrow = document.getElementById("rightArrow")
 let leftArrow = document.getElementById("leftArrow")
 let circles = document.querySelectorAll(".circle")
 circles[0].classList = "now-circle"
+
+
+// const hideLeftAnim = [
+//     { transform: "translateX(0%)", opacity: 1 },
+//     { transform: "translateX(-20%)", opacity: 0 },
+// ];
+//
+// const hideLeftAnimTiming = {
+//     duration: 1000,
+//     iterations: 1,
+// };
 rightArrow.addEventListener("click", ev => {
     let shownCard = document.getElementsByClassName("shown")[0]
     let shownCardIndex;
@@ -12,17 +23,30 @@ rightArrow.addEventListener("click", ev => {
         }
     }
     if (shownCardIndex === 3){
+        cards[shownCardIndex].classList = "slider-card rightAnim"
+        setTimeout(() => {
         cards[shownCardIndex].classList = "slider-card hidden"
         circles[shownCardIndex].classList = "circle"
         shownCardIndex = -1
-        cards[shownCardIndex+1].classList = "slider-card shown"
+            cards[shownCardIndex+1].classList = "slider-card leftAnimReversed"
+            setTimeout(() => {
+                cards[shownCardIndex+1].classList = "slider-card shown"
+            },1000)
         circles[shownCardIndex+1].classList = "now-circle"
+        },1000)
     }
     else {
+        cards[shownCardIndex].classList = "slider-card rightAnim"
+        setTimeout(() => {
         cards[shownCardIndex].classList = "slider-card hidden"
         circles[shownCardIndex].classList = "circle"
-        cards[shownCardIndex+1].classList = "slider-card shown"
+            cards[shownCardIndex+1].classList = "slider-card leftAnimReversed"
+            setTimeout(() => {
+                cards[shownCardIndex+1].classList = "slider-card shown"
+            },1000)
+
         circles[shownCardIndex+1].classList = "now-circle"
+        },1000)
     }
 
 
@@ -38,17 +62,30 @@ leftArrow.addEventListener("click", ev => {
         }
     }
     if (shownCardIndex === 0){
-        cards[shownCardIndex].classList = "slider-card hidden"
-        circles[shownCardIndex].classList = "circle"
-        shownCardIndex = 4
-        cards[shownCardIndex-1].classList = "slider-card shown"
-        circles[shownCardIndex-1].classList = "now-circle"
+        cards[shownCardIndex].classList = "slider-card leftAnim"
+        setTimeout(() => {
+            cards[shownCardIndex].classList = "slider-card hidden"
+            circles[shownCardIndex].classList = "circle"
+            shownCardIndex = 4
+            cards[shownCardIndex-1].classList = "slider-card rightAnimReversed"
+            setTimeout(() => {
+                cards[shownCardIndex-1].classList = "slider-card shown"
+            },1000)
+            circles[shownCardIndex-1].classList = "now-circle"
+        },1000)
     }
     else {
-        cards[shownCardIndex].classList = "slider-card hidden"
-        circles[shownCardIndex].classList = "circle"
-        cards[shownCardIndex-1].classList = "slider-card shown"
-        circles[shownCardIndex-1].classList = "now-circle"
+        cards[shownCardIndex].classList = "slider-card leftAnim"
+        setTimeout(() => {
+            cards[shownCardIndex].classList = "slider-card hidden"
+            circles[shownCardIndex].classList = "circle"
+            cards[shownCardIndex-1].classList = "slider-card rightAnimReversed"
+            setTimeout(() => {
+                cards[shownCardIndex-1].classList = "slider-card shown"
+            },1000)
+
+            circles[shownCardIndex-1].classList = "now-circle"
+        },1000)
     }
 
 })
